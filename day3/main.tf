d4c246b76f2356de81235882701694c466baec59
MohammeddJaveed <mdjaveed9108@gmail.com>
2026-06-09 12:53:51 +0100

    Initial Terraform code

---
+++ day3/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-west-1"
+
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0e564a15089b1ff77"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-04a7e449b3498e8f9"
+}
